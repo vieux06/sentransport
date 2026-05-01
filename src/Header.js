a48c032cf1f1ced97fa5dd3525024d704bca9560
Vieux Dame Leye <vieuxdameleye96@gmail.com>
2026-05-01 20:25:02 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,15 @@
 import './Header.css';
 
 function Header() {
+  const dateAujourdhui = new Date().toLocaleDateString('fr-FR');
+
   return (
     <header className="header">
       <h1 className="header-titre">SenTransport</h1>
       <p className="header-soustitre">
         Votre guide du transport en commun a Dakar
       </p>
+      <p className="header-date">{dateAujourdhui}</p>
     </header>
   );
 }
